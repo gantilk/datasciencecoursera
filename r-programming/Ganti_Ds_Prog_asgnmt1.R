dc3eb427c3b9face9aa46fc9821d1e95b17d8c3d
L Ganti <gantilk@gmail.com>
2016-02-27 08:17:51 -0500

    added corr() function

--- r-programming/Ganti_Ds_Prog_asgnmt1.R
+++ r-programming/Ganti_Ds_Prog_asgnmt1.R
@@ -48,4 +48,27 @@ complete <- function(directory, id = 1:332)
     outputdf
 }
 
-
+corr <- function (directory, threshold = 0)
+{
+    ## get the files from a given directory into a list
+    files.list <- list.files(directory,full.names=TRUE); ## Note this does not
+    ## take into account any sub-directories present in the directory
+    
+    # loop throug the files in the directory
+    correlationvector <- vector()
+    for(i in 1:length(files.list))
+    {
+        # find out whether we should consider the csv file with the current id
+        # if so, get the nitrate and sulfate columns as vectors and add to the 
+        # global sulfate and nitrate vectors
+        df = read.csv(files.list[i],header=TRUE)
+        good <-  complete.cases(df)
+        if(sum(good) > threshold) { 
+            completecases <- df[good,]
+            sulfatevector <- completecases[,"sulfate"]
+            nitratevector <- completecases[,"nitrate"]
+            append(cor(sulfatevector,nitratevector),correlationvector)
+            }
+    }
+    correlationvector
+}
